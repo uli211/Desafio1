1680093f29ef13d11671b0efcff00ec48f8acdaa
Ulises <ulises.rattin@gmail.com>
2024-08-12 10:57:56 -0300

    Implementación de la operación Multiplication

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,16 +1,4 @@
-﻿Console.WriteLine(Addition.Add(7, 2));
-Console.WriteLine(Substraction.Substract(10, 1));
-Console.WriteLine(Multiplication.Multiply(3, 3));
-Console.WriteLine(Division.Divide(18, 2));
-
-// Esta clase implementa la operación Addition
-public class Addition
-{
-    public static int Add(int a, int b)
-    {
-        return a + b;
-    }
-}
+﻿Console.WriteLine(Multiplication.Multiply(3, 3));
 // Esta clase implementa la operación Addition
 public class Multiplication
 {
