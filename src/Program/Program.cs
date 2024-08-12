e83ab0b9f3201196c24a60552a5b27077b96774d
Mrarchetti <brunitotennis@hotmail.com>
2024-08-12 10:34:53 -0300

    Resta

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine(Addition.Add(7, 2));
-Console.WriteLine(Substraction.Substract(10, 1));
-Console.WriteLine(Multiplication.Multiply(3, 3));
-Console.WriteLine(Division.Divide(18, 2));
+﻿Console.WriteLine(Substraction.Substract(10, 1));
+
+public class Substraction
+{
+    public static int Substract(int a, int b)
+    {
+        return a - b;
+    }
+}
