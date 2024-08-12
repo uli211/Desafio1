e2604767b6fcb1478d7dc905f4777b4f0c369e2c
Ulises <ulises.rattin@gmail.com>
2024-08-12 11:06:31 -0300

    Implementación de la operación <nombre de la operación>

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,3 @@
-
 Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
@@ -10,16 +9,18 @@ public class Addition
     public static int Add(int a, int b)
     {
         return a + b;
-public class Substraction
-{
-    public static int Substract(int a, int b)
-    {
-        return a - b;
     }
+}
 
-    
-}
-}
+public class Substraction
+    {
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
+
+    }
 // Esta clase implementa la operación Addition
 public class Multiplication
 {
