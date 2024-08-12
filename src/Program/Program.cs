c18ececf07b808c40291da5129c693eb2d93a0ca
Ulises <ulises.rattin@gmail.com>
2024-08-12 11:13:30 -0300

    Implementación de la operación Corregir

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,3 +1,35 @@
+Console.WriteLine(Addition.Add(7, 2));
+Console.WriteLine(Substraction.Substract(10, 1));
+Console.WriteLine(Multiplication.Multiply(3, 3));
+Console.WriteLine(Division.Divide(18, 2));
+
+// Esta clase implementa la operación Addition
+public class Addition
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+public class Substraction
+    {
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
+
+    }
+// Esta clase implementa la operación Addition
+public class Multiplication
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
+
 public class Division
 
 {
@@ -8,5 +40,4 @@ public class Division
 
 }
 
-}
 
